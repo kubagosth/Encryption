7ee647d650934ca0311f52832c501a29a8d60b9f
Jakub Grabarski (JAKU0065) <jaku0065@zbc.dk>
2022-06-21 17:30:21 +0200

    Upload Encrypter

---
+++ Encrypter/Encrypter.cs
@@ -0,0 +1,32 @@
+﻿namespace Encrypter
+{
+    public class Encrypter
+    {
+        public string Encrypt(string message)
+        {
+            string output = string.Empty;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                char c = message[i];
+                c++;
+                output += c;
+            }
+            return output;
+        }
+
+        public string Decrypt(string message)
+        {
+            string output = string.Empty;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                char c = message[i];
+                c--;
+                output += c;
+            }
+            return output;
+        }
+
+    }
+}
---
+++ Encrypter/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Encrypter.Encrypter encrypter = new Encrypter.Encrypter();
+
+string text = encrypter.Encrypt("Hello world");
+Console.WriteLine(text);
+text = encrypter.Decrypt(text);
+Console.WriteLine(text);
+
+Console.ReadLine();
+
